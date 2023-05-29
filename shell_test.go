97c43d34e28f1ea9ded76b33a9c60cad07d9ec5d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-05-29 17:54:20 +0700

    test: implement `requirePipeOutput` for shell command testing

--- shell_test.go
+++ shell_test.go
@@ -8,24 +8,36 @@ import (
 
 func TestRun(t *testing.T) {
 	t.Run("RunSuccessfully", func(t *testing.T) {
+		read, restore := requirePipeOutput(t)
+		defer restore()
 		err := Run("go", "version")
 		require.NoError(t, err)
+		require.Regexp(t, "^go version go\\S+ \\S+\n$", read())
 	})
 
 	t.Run("RunWithError", func(t *testing.T) {
+		read, restore := requirePipeOutput(t)
+		defer restore()
 		err := Run("go", "invalid")
 		require.ErrorContains(t, err, "exit status")
+		require.Regexp(t, "^go invalid: unknown command\nRun 'go help' for usage.\n$", read())
 	})
 }
 
 func TestRunSilently(t *testing.T) {
 	t.Run("RunSuccessfully", func(t *testing.T) {
+		read, restore := requirePipeOutput(t)
+		defer restore()
 		err := RunSilently("go", "version")
 		require.NoError(t, err)
+		require.Empty(t, read())
 	})
 
 	t.Run("RunWithError", func(t *testing.T) {
+		read, restore := requirePipeOutput(t)
+		defer restore()
 		err := RunSilently("go", "invalid")
 		require.ErrorContains(t, err, "exit status")
+		require.Empty(t, read())
 	})
 }
